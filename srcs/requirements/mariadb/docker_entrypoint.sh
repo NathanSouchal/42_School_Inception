616a573edf0eab0a0c5ad45271f9c5b6694f234e
Nathan Souchal <nsouchal@z3r12p3.42lyon.fr>
2024-11-15 15:12:46 +0100

    mariadb ok wordpress beginned

---
+++ srcs/requirements/mariadb/docker_entrypoint.sh
@@ -0,0 +1,16 @@
+set -e
+
+# Démarrer MariaDB en arrière-plan
+service mysql start
+
+# Exécuter les commandes SQL d'initialisation
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Arrêter MariaDB
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+# Démarrer MariaDB au premier plan
+exec mysqld
